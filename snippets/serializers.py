197b7a3a9582fe0e6e5da039a7ba03ee27fd1ba6
rulomoscoso <raul.moscoso84@gmail.com>
2023-02-07 11:18:39 -0600

    Updating our serializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -29,9 +29,11 @@ from django.contrib.auth.models import User
 #         return instance
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
         
 
 class UserSerializer(serializers.ModelSerializer):
