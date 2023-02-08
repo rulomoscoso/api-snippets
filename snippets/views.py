4e86243a5a8a59240bfb4c2aec7b255cbb82eab4
rulomoscoso <raul.moscoso84@gmail.com>
2023-02-08 12:43:54 -0600

    Final commit

--- snippets/views.py
+++ snippets/views.py
@@ -8,14 +8,6 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
-    
-
 class SnippetViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
@@ -27,13 +19,13 @@ class SnippetViewSet(viewsets.ModelViewSet):
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     
-    @action(detail=True, renderer_class=[renderers.StaticHTMLRenderer])
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
     
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user   )
+        serializer.save(owner=self.request.user)
     
 
 
